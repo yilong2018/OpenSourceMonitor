fc812a49cb7d0e824ca25d55bdbc30aecf17827f
Yi Long <peng@M1.com>
2021-04-30 18:42:43 +0800

    pushgateway simple sample

---
+++ prometheus/python/pushgateway.py
@@ -0,0 +1,10 @@
+from prometheus_client import CollectorRegistry, Gauge, push_to_gateway
+
+registry = CollectorRegistry()
+g = Gauge(
+    'pushgateway_job_last_success_unixtime',
+    'Last time a batch job successfully finished',
+    ['a', 'b'],
+    registry=registry)
+g.labels(a='1', b='2').set_to_current_time()
+push_to_gateway('localhost:9091', job='job test', registry=registry)
